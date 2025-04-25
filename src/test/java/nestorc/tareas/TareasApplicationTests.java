d1b348d84db2800cd673e39162cc83862f311022
Neo3691102 <nestor8772@gmail.com>
2025-04-24 18:24:49 -0600

    Configuracion inicial con conexion a la base de datos

---
+++ src/test/java/nestorc/tareas/TareasApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorc.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
